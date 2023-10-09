0b8db1d1e418f259f5bbe53b8ca29d84cfe99182
GauravDattuPise <GauravDattuPise>
2023-10-09 23:42:26 +0530

    development

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path")
+const cors = require("cors")
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 
 // connecting to mongodb
@@ -15,10 +17,22 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((err)=>console.log("error in connecting with db", err.message));
 
 // path to admin route
-app.use("/admin", require("./route/adminRoute"))
+app.use("/admin", require("./src/route/adminRoute"))
 
 // path to user route
-app.use("/user", require("./route/userRoute"));
+app.use("/user", require("./src/route/userRoute"));
+
+// static files
+// code for deployment of project
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  )
+});
 
 // application running on server
 app.listen(process.env.PORT, ()=>{
